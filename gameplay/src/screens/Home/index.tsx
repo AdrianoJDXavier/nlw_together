03694e54d6dc2a88ab9c4961e4026963270dffd6
AdrianoJDXavier <aj-dx@hotmail.com>
2021-06-22 13:51:51 -0300

    Corrigindo bugs e add restante da aula

--- gameplay/src/screens/Home/index.tsx
+++ gameplay/src/screens/Home/index.tsx
@@ -1,22 +1,79 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, FlatList } from 'react-native';
 import { styles } from './styles';
 
 import { CategorySelect } from '../../components/CategorySelect';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { Profile } from '../../components/Profile';
+import { Appointment } from '../../components/Appointment';
+import { ListDivider } from '../../components/ListDivider';
+import { ListHeader } from '../../components/ListHeader';
+
 
 export function Home() {
-    return(
-        <View>
-            <View style={styles.header}>
-                <Profile />
-                <ButtonAdd />
-            </View>
-            <View>
-                
-            </View>
+    const [category, setCategory] = useState('');
+
+    const appointments = [
+      {
+        id: '1',
+        guild: {
+          id: '1',
+          name: 'Lendários',
+          icon: null,
+          owner: true
+        },
+        category: '1',
+        date: '21/06 às 13:40h',
+        description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+      },
+      {
+        id: '2',
+        guild: {
+          id: '1',
+          name: 'Lendários',
+          icon: null,
+          owner: true
+        },
+        category: '1',
+        date: '21/06 às 13:47h',
+        description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+      },
+
+    ]
+
+    function handleCategorySelect(categoryId: string) {
+      categoryId === category ? setCategory('') : setCategory(categoryId);
+    }
+
+    return (
+      <View>
+        <View style={styles.header}>
+          <Profile />
+          <ButtonAdd />
         </View>
+
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect}
+        />
+
+        <View style={styles.content}>
+          <ListHeader
+            title="Partidas agendadas"
+            subtitle="Total 6"
+          />
+
+          <FlatList
+              data={appointments}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) => (
+              <Appointment data={item} />
+            )}
+            ItemSeparatorComponent={() => <ListDivider />}
+            style={styles.matches}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
+      </View>
     );
-    
-}
+  }
